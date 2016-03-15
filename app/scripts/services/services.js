6a4ba2822ded534e158a50e112a301892237338a
cardaminexhz <zhxh69776@qq.com>
2016-03-15 21:48:15 +0800

    echarts3.实现饼图；数据通过JSONP从远程js文件获取

--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -1,3 +1,21 @@
 'use strict';
 
 var services = angular.module('EchartsDemo.services', []);
+
+// JSONP访问8090端口的远程文件piedata.js，获取饼图数据
+services.factory('PieDataLoader', ['$http', '$q',
+    function($http, $q) {
+        return function() {
+            var piedata = {};
+            var pieUrl = "http://localhost:8090/piedata.js?callback=JSON_CALLBACK";
+            var delay = $q.defer();
+            $http.jsonp(pieUrl).success(function(data) {
+                console.log("get data success");
+                piedata = data;
+                delay.resolve(piedata);
+            }, function() {
+                delay.reject('Unable to fetch recipes');
+            });
+            return delay.promise;
+        };
+    }]);
